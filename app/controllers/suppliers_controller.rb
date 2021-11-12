52389dd4f188de04c9fbd77d421ad9595561b831
David Watts <davidwatts11@gmail.com>
2021-11-12 23:59:25 +1000

    added new supplier feature

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -4,6 +4,7 @@ class SuppliersController < ApplicationController
   end
 
   def new
+    @supplier = Supplier.new
   end
 
   def edit
@@ -12,6 +13,20 @@ class SuppliersController < ApplicationController
   def show
   end
 
+  def create
+    @supplier = Supplier.new(supplier_params)
+    @supplier.save
+    redirect_to suppliers_path
+  end
+
   def destroy
   end
+
+  private
+
+  ## Todo add relations 
+  def supplier_params
+    params.require(:supplier).permit(:display_name, :business_name, :business_num, :accred_num, :phone, :display_email, :quote_email, :instant_price, 
+    :short_description, :long_description, :short_description, :website, :address)
+  end 
 end
