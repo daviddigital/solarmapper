9b1760f12f19d83eb68aa5e892c27184eb1dc0f9
David Watts <davidwatts11@gmail.com>
2021-11-20 10:37:11 +1000

    added postcode filter if not in params

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ phone: "07 5000 5040",
 display_email: "contact@dwsolar.com",
 quote_email: "davidwatts11@gmail.com",
 instant_price: true,
-short_description: "Serving Brisbane since 1986, we install solar and batteries in QLD.",
+short_description: "Serving Brisbane since 1986, we install solar and batteries in QLD. Brands include Jino Solar panels and Tesla Powerwall",
 long_description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 website: "dwsolar.com")
 
